1e689343a1ac1887e8e3e2b3d2aa028d038ba936
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-27 16:30:38 +0100

    Aggiunta immagine upload nel file show

--- resources/views/Admin/projects/show.blade.php
+++ resources/views/Admin/projects/show.blade.php
@@ -6,6 +6,11 @@
             <h1>{{ $project->title }}</h1>
         </div>
         <div class="my-4">
+            <div class="text-center">
+                @if ($project->$cover_image)
+                    <img class="w-25" src="{{ asset("storage/$project->$cover_image") }}" alt="{{ $project->$title }}">
+                @endif
+            </div>
             <p>{{ $project->description }}</p>
         </div>
     </div>
